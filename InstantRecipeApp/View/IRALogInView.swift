201627dd7e8648bd77b02c3447704139c3c47b0b
chandMessi10 <surajchand3010@gmail.com>
2024-02-21 23:41:02 +0545

    first commit

---
+++ InstantRecipeApp/View/IRALogInView.swift
@@ -0,0 +1,18 @@
+//
+//  IRALoginView.swift
+//  InstantRecipeApp
+//
+//  Created by Suraj Chand on 21/02/2024.
+//
+
+import SwiftUI
+
+struct IRALoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IRALoginView()
+}
---
+++ InstantRecipeApp/ViewModels/IRALogInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  IRALogInViewModel.swift
+//  InstantRecipeApp
+//
+//  Created by Suraj Chand on 21/02/2024.
+//
+
+import Foundation
